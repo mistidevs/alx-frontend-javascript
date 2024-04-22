f4708d12d81c216bebfa6484348e563678e6872d
mistidevs <mistidevs@gmail.com>
2024-04-22 13:03:06 +0300

    code (./0x00*/8-getBudgetCurrentYear.js) : Inverting order of key and value

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -5,9 +5,9 @@ function getCurrentYear() {
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
-    [income]: `income-${getCurrentYear()}`,
-    [gdp]: `gdp-${getCurrentYear()}`,
-    [capita]: `capita-${getCurrentYear()}`
+    [`income-${getCurrentYear()}`]: [income],
+    [`gdp-${getCurrentYear()}`]: [gdp],
+    [`capita-${getCurrentYear()}`]: [capita] 
   };
 
   return budget;
