e886fc182061a682c0a4d182f92f72faf8656a0d
mistidevs <mistidevs@gmail.com>
2024-04-23 13:12:07 +0300

    code (100-createIteratorObject.js) : Eslint compliance

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,10 +1,10 @@
 export default function createIteratorObject(report) {
   const myVals = Object.values(Object.values(report.allEmployees));
 
-  const myList = []
+  const myList = [];
   for (const idx of myVals) {
     for (const index of idx) {
-      myList.push(index)
+      myList.push(index);
     }
   }
 
