6374454d1467722bf6e6f813f7b1ce2cc8ed7afa
mistidevs <mistidevs@gmail.com>
2024-04-23 11:30:20 +0300

    code (./0x00*/) : Refactoring functions to be ALX Checker Compliant

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const [idx, value] of array.entries()) {
-    array[idx] = appendString + value;
+  const arr = [];
+  for (const idx of array) {
+    const value = idx
+    arr.push(appendString + value)
   }
 
-  return array;
+  return arr;
 }
