184e65bbe49aa9412bfa9e545881bd2e1e9d49e4
mistidevs <mistidevs@gmail.com>
2024-04-23 13:04:49 +0300

    code (100-createIteratorObject.js) : Removing Symbol.iterator

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -8,22 +8,5 @@ export default function createIteratorObject(report) {
     }
   }
 
-  myList[Symbol.iterator] = function() {
-    const keys = Object.keys(this);
-    let index = 0;
-    
-    return {
-      next: () => {
-        if (index < keys.length) {
-          const key = keys[index];
-          index++;
-          return { value: [key, this[key]], done: false };
-        } else {
-          return { done: true };
-        }
-      }
-    };
-  };
-
   return myList;
 }
