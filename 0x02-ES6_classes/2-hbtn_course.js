83e1742df178d39ddb73c5ece25127242a62ca99
mistidevs <mistidevs@gmail.com>
2024-04-26 11:04:56 +0300

    code (2-hbtn_course.js) : Error Handling

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -34,18 +34,24 @@ export default class HolbertonCourse {
   set name(newName) {
     if (newName instanceof String) {
       this._name = newName;
+    } else {
+      throw new TypeError('Name must be a string');
     }
   }
 
   set length(newLength) {
     if (newLength instanceof Number) {
       this._length = newLength;
+    } else {
+      throw new TypeError('Length must be a number');
     }
   }
 
   set students(newStudents) {
     if (HolbertonCourse.isArrayofStrings(newStudents) === true) {
       this._students = newStudents;
+    } else {
+      throw new TypeError('Students must be an array of strings');
     }
   }
 }
