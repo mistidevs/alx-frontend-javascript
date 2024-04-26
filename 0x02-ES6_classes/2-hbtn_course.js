efed311ae24ffafd751e4aca52975c43b55b6f82
mistidevs <mistidevs@gmail.com>
2024-04-26 11:07:27 +0300

    code (2-hbtn_course.js) : Refactoring verification to not terminate execution

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,57 +1,52 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
-  }
-
-  static isArrayofStrings(value) {
-    if (!Array.isArray(value)) {
-      return false;
-    }
-
-    for (const i of value.length) {
-      if (typeof value[i] !== 'string') {
-        return false;
-      }
-    }
-
-    return true;
+      this._name = this._verifyString(name, 'Name');
+      this._length = this._verifyNumber(length, 'Length');
+      this._students = this._verifyArrayOfStrings(students, 'Students');
   }
 
   get name() {
-    return this._name;
-  }
-
-  get length() {
-    return this._length;
-  }
-
-  get students() {
-    return this._students;
+      return this._name;
   }
 
   set name(newName) {
-    if (newName instanceof String) {
-      this._name = newName;
-    } else {
-      throw new TypeError('Name must be a string');
-    }
+      this._name = this._verifyString(newName, 'Name');
+  }
+
+  get length() {
+      return this._length;
   }
 
   set length(newLength) {
-    if (newLength instanceof Number) {
-      this._length = newLength;
-    } else {
-      throw new TypeError('Length must be a number');
-    }
+      this._length = this._verifyNumber(newLength, 'Length');
+  }
+
+  get students() {
+      return this._students;
   }
 
   set students(newStudents) {
-    if (HolbertonCourse.isArrayofStrings(newStudents) === true) {
-      this._students = newStudents;
-    } else {
-      throw new TypeError('Students must be an array of strings');
-    }
+      this._students = this._verifyArrayOfStrings(newStudents, 'Students');
+  }
+
+  _verifyString(value, propName) {
+      if (typeof value !== 'string') {
+          throw new TypeError(`${propName} must be a string`);
+      }
+      return value;
+  }
+
+  _verifyNumber(value, propName) {
+      if (typeof value !== 'number') {
+          throw new TypeError(`${propName} must be a number`);
+      }
+      return value;
+  }
+
+  _verifyArrayOfStrings(value, propName) {
+      if (!Array.isArray(value) || value.some(item => typeof item !== 'string')) {
+          throw new TypeError(`${propName} must be an array of strings`);
+      }
+      return value;
   }
 }
