593bb0c9f8d216cbb5b53044210a8c8dcc8e2f4e
mistidevs <mistidevs@gmail.com>
2024-04-26 16:46:28 +0300

    code (4-pricing.js) : Adding type checking

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,30 +1,50 @@
-import Currency from "./3-currency";
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    } else if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of class Currency');
+    }
+
     this._amount = amount;
     this._currency = currency;
   }
 
   get amount() {
-    this._amount;
+    return this._amount;
   }
+
   get currency() {
-    this._currency;
+    return this._currency;
   }
 
   set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
     this._amount = amount;
   }
+
   set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of class Currency');
+    }
     this._currency = currency;
   }
 
   displayFullPrice() {
-    return (`${this._amount} ${this._currency._name} (${this._currency._code})`);
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    } else if (typeof conversionRate !== 'number') {
+      throw new TypeError('Conversion rate must be a number');
+    }
+
     return amount * conversionRate;
   }
 }
