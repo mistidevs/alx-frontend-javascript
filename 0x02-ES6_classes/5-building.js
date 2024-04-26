ba724557ca3c92b4c2edaacb3b6cd62a79574f7a
mistidevs <mistidevs@gmail.com>
2024-04-26 15:59:20 +0300

    code (5-building.js) : Refactoring Building Class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,14 @@
 export default class Building {
-  constructor(sqft) {
-    this._sqft = sqft;
-  }
+  constructor(sqft = 0) {
+    if (this.constructor !== Building
+        && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
 
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(sqft) {
-    this._sqft = sqft;
   }
 }
