0316b25a3837d06b21cc15ec03377b5d1039d5cd
mistidevs <mistidevs@gmail.com>
2024-04-26 15:24:05 +0300

    code (4-pricing.js) : Pricing Class

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,30 @@
+import Currency from "./3-currency";
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    this._amount;
+  }
+  get currency() {
+    this._currency;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return (`${this._amount} ${this._currency._name} ${this._currency._code}`);
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
