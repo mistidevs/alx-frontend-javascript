d6197f70376a9f914c235f1d41a7e1f6a19872da
mistidevs <mistidevs@gmail.com>
2024-04-26 10:25:40 +0300

    code (1-make_classrooms.js) : Initializing Classrooms

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,9 @@
+import ClassRoom from "./0-classroom.js";
+
+export default class initializeRooms extends ClassRoom {
+  constructor() {
+    ClassRoom(19),
+    ClassRoom(20),
+    ClassRoom(34)
+  }
+}
