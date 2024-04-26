499e3a5561e81ba0e5185f64a5b3c95154092146
mistidevs <mistidevs@gmail.com>
2024-04-26 13:44:47 +0300

    code (2-hbtn_course.js) : Using static methods

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,52 +1,52 @@
 export default class HolbertonCourse {
-constructor(name, length, students) {
-  this._name = this._verifyString(name, 'Name');
-  this._length = this._verifyNumber(length, 'Length');
-  this._students = this._verifyArrayOfStrings(students, 'Students');
-}
-
-get name() {
-  return this._name;
-}
-
-set name(newName) {
-  this._name = this._verifyString(newName, 'Name');
-}
-
-get length() {
-  return this._length;
-}
-
-set length(newLength) {
-  this._length = this._verifyNumber(newLength, 'Length');
-}
-
-get students() {
-  return this._students;
-}
-
-set students(newStudents) {
-  this._students = this._verifyArrayOfStrings(newStudents, 'Students');
-}
-
-_verifyString(value, propName) {
-  if (typeof value !== 'string') {
-    throw new TypeError(`${propName} must be a string`);
+  constructor(name, length, students) {
+    this._name = this.HolbertonCourse._verifyString(name, 'Name');
+    this._length = this.HolbertonCourse._verifyNumber(length, 'Length');
+    this._students = this.HolbertonCourse._verifyArrayOfStrings(students, 'Students');
   }
-  return value;
-}
 
-_verifyNumber(value, propName) {
-  if (typeof value !== 'number') {
-    throw new TypeError(`${propName} must be a number`);
+  get name() {
+    return this._name;
   }
-  return value;
-}
 
-_verifyArrayOfStrings(value, propName) {
-  if (!Array.isArray(value) || value.some(item => typeof item !== 'string')) {
-    throw new TypeError(`${propName} must be an array of strings`);
+  set name(newName) {
+    this._name = this.HolbertonCourse._verifyString(newName, 'Name');
   }
-  return value;
-}
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    this._length = this.HolbertonCourse._verifyNumber(newLength, 'Length');
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    this._students = this.HolbertonCourse._verifyArrayOfStrings(newStudents, 'Students');
+  }
+
+  static _verifyString(value, propName) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${propName} must be a string`);
+    }
+    return value;
+  }
+
+  static _verifyNumber(value, propName) {
+    if (typeof value !== 'number') {
+      throw new TypeError(`${propName} must be a number`);
+    }
+    return value;
+  }
+
+  static _verifyArrayOfStrings(value, propName) {
+    if (!Array.isArray(value) || value.some(item => typeof item !== 'string')) {
+      throw new TypeError(`${propName} must be an array of strings`);
+    }
+    return value;
+  }
 }
