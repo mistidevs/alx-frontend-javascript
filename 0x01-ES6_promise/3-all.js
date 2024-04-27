a524eb8b7cdced3ed15e3a5dbed11312c65b89df
mistidevs <mistidevs@gmail.com>
2024-04-27 21:53:27 +0300

    code (./0x01-*) : Refactoring 2 and 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,10 +2,9 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    .then((value) => {
+      console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
     })
-    .catch(() => {
-      console.error('Signup system offline');
-    });
+
+    .catch(() => console.log('Signup system offline'));
 }
