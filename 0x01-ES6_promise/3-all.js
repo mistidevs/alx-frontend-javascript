01a57ad5027fb47ecf5d8c34e882a02f2c966152
mistidevs <mistidevs@gmail.com>
2024-04-27 21:47:34 +0300

    code (3-all.js) : ESLint Fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import { uploadPhoto, createUser } from './utils'
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-  .then(([photoResponse, userResponse]) => {
-    console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
-  })
-  .catch (() => {
-    console.error("Signup system offline")
-  });
+    .then(([photoResponse, userResponse]) => {
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch(() => {
+      console.error('Signup system offline');
+    });
 }
